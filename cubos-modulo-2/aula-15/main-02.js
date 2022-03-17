59639db3ec1c2dbdff8d65b1f4f1c7fb59a7dcbd
PedroCicc <pedrocicc14@gmail.com>
2022-03-17 02:50:46 -0300

    commit: added new functionalities to choice the beer format.

--- cubos-modulo-2/aula-15/main-02.js
+++ cubos-modulo-2/aula-15/main-02.js
@@ -1,6 +1,7 @@
 const adultos = document.querySelector(".adultos");
 const criancas = document.querySelector(".criancas");
 const button = document.querySelector("button");
+const select = document.querySelector("select");
 
 button.addEventListener("click", () => {
 
@@ -12,13 +13,38 @@ button.addEventListener("click", () => {
     let refrigerante = qtdAdultos + qtdCriancas * 0.5;
     let cerveja = qtdAdultos * 0.5;
 
+    let formatoDeCerveja;
+    let fraseCerveja = "";
+
+    if (select.value === "Garrafa de 600ml") {
+        formatoDeCerveja = Math.round(cerveja / 0.6);
+        fraseCerveja = `${formatoDeCerveja} garrafas de 600ml`
+    } else if (select.value === "Latão 475ml") {
+        formatoDeCerveja = Math.round(cerveja / 0.475);
+        fraseCerveja = `${formatoDeCerveja} latões de 475ml`
+    } else if (select.value === "Long neck 330ml") {
+        formatoDeCerveja = Math.round(cerveja / 0.33);
+        fraseCerveja = `${formatoDeCerveja} long necks de 330ml`
+    } else if (select.value === "Lata 350ml") {
+        formatoDeCerveja = Math.round(cerveja / 0.35);
+        fraseCerveja = `${formatoDeCerveja} latas de 350ml`
+    } else {
+        formatoDeCerveja = Math.round(cerveja / 0.475);
+        fraseCerveja = `${formatoDeCerveja} latinhas de 269ml`
+    }
+
     alert(`Lista de compras:
     - ${carne}g de carne
     - ${coracao}g de coração
-    - ${cerveja}L de cerveja
+    - ${fraseCerveja} de cerveja
     - ${refrigerante}L de refrigerante`);
 });
 
+// Garrafa de 600ml
+// Latão 475ml
+// Long neck 330ml
+// Lata 350ml
+// Latinha 269ml
 
 // Por adulto:
 
