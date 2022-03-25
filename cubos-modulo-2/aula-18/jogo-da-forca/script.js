29ed4fc2a6d92d44fe8b8cc9ef0507627d3bbe35
PedroCicc <pedrocicc14@gmail.com>
2022-03-25 16:24:16 -0300

    commit: copied hangman exercise basis.

---
+++ cubos-modulo-2/aula-18/jogo-da-forca/script.js
@@ -0,0 +1,15 @@
+function gerarNumeroInteiroAleatorio(min, max) {
+	// número fracionário aleatório maior ou igual a 0 e menor que 1
+	const aleatorioDeBase = Math.random();
+	// número fracionário aleatório maior ou igual a 0 e menor que (max - min + 1)
+	const aleatorioFracionario = Math.random() * (max - min + 1);
+	// número inteiro aleatório maior ou igual a 0 e menor ou igual a (max - min)
+	// Math.trunc tira a parte fracionária de um número: 0,5 vira 0, 1,25 vira 1, etc
+	const aleatorioInteiro = Math.trunc(aleatorioFracionario);
+	// número inteiro aleatório maior ou igual a min e menor ou igual a max
+	return min + aleatorioInteiro;
+}
+
+function escolherElementoAleatorio(array) {
+	return array[gerarNumeroInteiroAleatorio(0, array.length - 1)]
+}
