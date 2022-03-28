94ab7b9a0e2b20453ad7386cff9f6905f0e8fbb2
PedroCicc <pedrocicc14@gmail.com>
2022-03-28 11:56:55 -0300

    commit: finished hangman exercise.

--- cubos-modulo-2/aula-18/jogo-da-forca/script.js
+++ cubos-modulo-2/aula-18/jogo-da-forca/script.js
@@ -1,3 +1,7 @@
+const partesDoCorpo = document.querySelectorAll(".corpo *");
+const elementoLetras = document.querySelector(".letras");
+const botoes = document.querySelectorAll(".botoes button");
+
 function gerarNumeroInteiroAleatorio(min, max) {
 	// número fracionário aleatório maior ou igual a 0 e menor que 1
 	const aleatorioDeBase = Math.random();
@@ -13,3 +17,97 @@ function gerarNumeroInteiroAleatorio(min, max) {
 function escolherElementoAleatorio(array) {
 	return array[gerarNumeroInteiroAleatorio(0, array.length - 1)]
 }
+
+const palavras = ["palmeiras", "corinthians", "botafogo", "sao paulo"];
+
+let palavraSorteada;
+let qtdAcertos = 0;
+let qtdErros = 0;
+
+function iniciarPartida() {
+	qtdAcertos = 0;
+	qtdErros = 0;
+	palavraSorteada = escolherElementoAleatorio(palavras);
+
+	for (const parteDoCorpo of partesDoCorpo) {
+		parteDoCorpo.setAttribute("hidden", "");
+	}
+
+	elementoLetras.innerHTML = "";
+
+	for (const letra of palavraSorteada.split("")) {
+		const elementoLetra = document.createElement("span");
+		elementoLetra.classList.add("letra");
+		elementoLetras.append(elementoLetra);
+	}
+}
+
+iniciarPartida();
+
+function chutarLetra(letra) {
+	const acertou = palavraSorteada.includes(letra);
+
+	if (acertou) {
+		const letras = document.querySelectorAll(".letra");
+
+		for (let i = 0; i < palavraSorteada.length; i++) {
+			if (palavraSorteada[i] === letra) {
+				letras[i].innerText = letra;
+				qtdAcertos++;
+			}
+		}
+
+		const ganhou = qtdAcertos === palavraSorteada.length;
+
+		if (ganhou) {
+			alert("Parabéns!! Você ganhou!");
+			iniciarPartida();
+		}
+	} else {
+		partesDoCorpo[qtdErros].removeAttribute("hidden");
+		qtdErros++;
+
+		const perdeu = qtdErros === partesDoCorpo.length;
+
+		if(perdeu) {
+			alert(`Poxa! Você perdeu. A palavra era "${palavraSorteada}".`);
+			iniciarPartida();
+		}
+	}
+}
+
+for (botao of botoes) {
+	botao.addEventListener("click", event => {
+		const botaoClicado = event.target;
+		const letra = botaoClicado.innerText;
+		chutarLetra(letra);
+	})
+}
+
+document.addEventListener("keypress", event => {
+	const letra = event.key;
+	chutarLetra(letra);
+})
+
+
+// Lógica do programa
+
+// Jogo dividido em partidas
+
+// Início da partida
+// 		Sortear uma palavra //
+// 		Esconder as partes do corpo //
+//		Colocar os tracinhos das letras da palavra //
+
+// Loop do jogo
+//		Usuário chuta uma letra //
+//		Se acertar
+//			Revelar todas as ocorrências daquela letra na palavra sorteada; //
+//			Se todas as letras já tiverem sido reveladas, o usuário ganha. //
+//				Mostrar mensagem de parabéns; //
+//				Reiniciar a partida. //
+//		Se errar
+//			Revelar uma parte do corpo; //
+//			Se todas as partes do corpo já tiverem sido reveladas, o usuário perde. //
+//				Mostrar mensagem; //
+//				Reiniciar a partida. //
